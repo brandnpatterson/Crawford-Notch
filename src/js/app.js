a4270abb5e5ff19f73beb27e499107f34cec7241
Brandon Patterson <brandonnpatterson@gmail.com>
2017-04-14 22:10:01 -0500

    added fade-in on scroll

--- src/js/app.js
+++ src/js/app.js
@@ -11,16 +11,19 @@ $(document).ready(function crawfordNotch() {
       this.bindEvents();
     },
     cacheDOM () {
+      this.$aboutImg = $('.about-img');
       this.$document = $(document);
       this.$dropBtn = $('.drop-btn');
+      this.$instafeed = $('#instafeed');
       this.$modal = $('.modal');
       this.$nav = $('nav');
+      this.$selectionH2 = $('.selection-h2');
       this.$window = $(window);
     },
     bindEvents () {
       this.$dropBtn.on('click', this.toggleDropBtn.bind(this));
       this.$nav.on('click', this.toggleDropBtn.bind(this));
-      this.$window.scroll(this.toggleNavbar.bind(this));
+      this.$window.scroll(this.listenForScroll.bind(this));
       this.$window.resize(this.onResizeBrowser.bind(this));
     },
     toggleDropBtn () {
@@ -31,7 +34,7 @@ $(document).ready(function crawfordNotch() {
         this.$modal.toggleClass('visible');
       }
     },
-    toggleNavbar () {
+    listenForScroll () {
       if (this.$document.scrollTop() > 10 && this.$window.width() > 724) {
         this.$nav
         .addClass('nav-desktop')
@@ -40,6 +43,15 @@ $(document).ready(function crawfordNotch() {
         this.$nav
         .addClass('modal')
         .removeClass('nav-desktop');
+      }
+      if (this.$document.scrollTop() > 250) {
+        this.$aboutImg.addClass('fade-in');
+      }
+      if (this.$document.scrollTop() > 700) {
+        this.$selectionH2.addClass('fade-in');
+      }
+      if (this.$document.scrollTop() > 1700) {
+        this.$instafeed.addClass('fade-in');
       }
     },
     onResizeBrowser () {
--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,6 @@
  *  Webpack entry point
 **/
 
-import './spinner.js';
+import './page-load.js';
 import './app.js';
 import './instafeed.js';
