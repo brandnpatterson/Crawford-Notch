93fdf34bf4b91cd3f23ec5acaa614ab95dad770d
Brandon Patterson <brandonnpatterson@gmail.com>
2017-04-19 10:42:20 -0500

    simplified logic for dropdown

--- src/js/app.js
+++ src/js/app.js
@@ -18,7 +18,6 @@ $(document).ready(function crawfordNotch() {
       this.$document = $(document);
       this.$dropBtn = $('.drop-btn');
       this.$instafeed = $('#instafeed');
-      this.$modal = $('.modal');
       this.$nav = $('nav');
       this.$selection = $('#selection');
       this.$window = $(window);
@@ -27,26 +26,16 @@ $(document).ready(function crawfordNotch() {
       this.$dropBtn.on('click', this.toggleDropBtn.bind(this));
       this.$nav.on('click', this.toggleDropBtn.bind(this));
       this.$window.scroll(this.listenForScroll.bind(this));
-      this.$window.resize(this.onResizeBrowser.bind(this));
     },
     toggleDropBtn () {
-      if (this.$nav.is('.modal')) {
+      if (this.$dropBtn.is(':visible')) {
         this.$dropBtn
         .toggleClass('drop-btn-x')
         .toggleClass('drop-btn-hamburger');
-        this.$modal.toggleClass('visible');
+        this.$nav.toggleClass('visible');
       }
     },
     listenForScroll () {
-      if (this.$document.scrollTop() > 10 && this.$window.width() > $medium) {
-        this.$nav
-        .addClass('nav-desktop')
-        .removeClass('modal');
-      } else if (this.$document.scrollTop() <= 5 && this.$window.width() > $medium) {
-        this.$nav
-        .addClass('modal')
-        .removeClass('nav-desktop');
-      }
       if (this.$document.scrollTop() > 230) {
         this.$aboutImg.addClass('fade-in');
       }
@@ -55,13 +44,6 @@ $(document).ready(function crawfordNotch() {
       }
       if (this.$document.scrollTop() > 1700) {
         this.$instafeed.addClass('fade-in');
-      }
-    },
-    onResizeBrowser () {
-      if (this.$window.width() < 724) {
-        this.$nav
-        .addClass('modal')
-        .removeClass('nav-desktop');
       }
     }
   };
