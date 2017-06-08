66ccb797ceae8ac5d2d1600e17c6dd5b0ef3b861
Brandon Patterson <brandonnpatterson@gmail.com>
2017-06-08 05:12:16 -0500

    generated a fresh accessToken - was not populating

--- src/js/instafeed.js
+++ src/js/instafeed.js
@@ -7,7 +7,7 @@ import Instafeed from 'instafeed.js';
 const userFeed = new Instafeed ({
   get: 'user',
   userId: 3478504949,
-  accessToken: '3478504949.1677ed0.b143766a52f14d0da5c2fc32891ac53b',
+  accessToken: '3478504949.1677ed0.ce4db999626a4f9ab7c2a4a36602f939',
   resolution: 'standard_resolution'
 });
 userFeed.run();
